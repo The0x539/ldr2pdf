fc11879c03e5e54ca67438cd7329edb805bfc6fe
The0x539 <the0x539@gmail.com>
2025-04-07 14:22:20 -0500

    Refactor into modules

---
+++ render_3d/src/primitives.rs
@@ -0,0 +1,182 @@
+use bevy_lines::prelude::*;
+use ldr2pdf_common::ldr::{
+    CURRENT_COLOR, ColorCode, ColorMap, GeometryContext, Winding, new_color,
+};
+use std::collections::HashMap;
+use weldr::{Command, SourceMap};
+
+use bevy::{
+    asset::RenderAssetUsages,
+    prelude::*,
+    render::mesh::{Indices, PrimitiveTopology},
+};
+
+use crate::bevy_from_weldr;
+
+#[derive(Default)]
+pub struct Primitives {
+    triangles: Vec<Triangle3d>,
+    triangle_colors: HashMap<usize, ColorCode>,
+    lines: Vec<[Vec3; 2]>,
+    opt_lines: Vec<([Vec3; 2], [Vec3; 2])>,
+}
+
+impl Primitives {
+    pub fn of_part(source_map: &SourceMap, model_name: &str) -> Self {
+        let mut primitives = Self::default();
+        let mut ctx = GeometryContext::new();
+        ctx.transform = weldr::Mat4::IDENTITY;
+        traverse_part(source_map, model_name, ctx, &mut primitives);
+        primitives
+    }
+
+    pub fn build_mesh(&self, color_map: &ColorMap, main_color: ColorCode) -> Mesh {
+        let mut positions = Vec::<Vec3>::new();
+        let mut normals = Vec::<Vec3>::new();
+        let mut colors = Vec::<Vec4>::new();
+        let mut indices = Indices::U16(vec![]);
+        let mut dedup = HashMap::<([u32; 3], [u32; 3], u32), u32>::new();
+
+        // We want to use indexed vertices for memory efficiency,
+        // but we also (usually?) want flat normals,
+        // so we need to compute flat normals ourselves
+        // and duplicate the vertex for each face it belongs to
+        // TODO: Identify cases where we do want smooth normals
+
+        for (triangle_index, triangle) in self.triangles.iter().enumerate() {
+            let color_code = *self
+                .triangle_colors
+                .get(&triangle_index)
+                .unwrap_or(&main_color);
+
+            let c = color_map.by_code(color_code).value;
+            let color = Color::srgb_u8(c.red, c.green, c.blue).to_srgba().to_vec4();
+
+            let normal = triangle.normal().unwrap_or(Dir3::X).as_vec3();
+            for vertex in triangle.vertices {
+                let key = (bytemuck::cast(vertex), bytemuck::cast(normal), color_code);
+
+                let index = *dedup.entry(key).or_insert_with(|| {
+                    let i = positions.len() as u32;
+                    positions.push(vertex);
+                    normals.push(normal);
+                    if !self.triangle_colors.is_empty() {
+                        colors.push(color);
+                    }
+                    i
+                });
+
+                indices.push(index);
+            }
+        }
+
+        let mut mesh = Mesh::new(
+            PrimitiveTopology::TriangleList,
+            RenderAssetUsages::default(),
+        );
+        mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+        mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+        if !self.triangle_colors.is_empty() {
+            mesh.insert_attribute(Mesh::ATTRIBUTE_COLOR, colors);
+        }
+        mesh.insert_indices(indices);
+        mesh
+    }
+
+    pub fn build_lines(&self) -> Polyline {
+        Polyline {
+            vertices: self.lines.iter().copied().flatten().collect(),
+            control_vertices: None,
+        }
+    }
+
+    pub fn build_opt_lines(&self) -> Polyline {
+        Polyline {
+            vertices: self.opt_lines.iter().flat_map(|v| v.0).collect(),
+            control_vertices: Some(self.opt_lines.iter().flat_map(|v| v.1).collect()),
+        }
+    }
+}
+
+fn traverse_part(
+    source_map: &SourceMap,
+    model_name: &str,
+    ctx: GeometryContext,
+    output: &mut Primitives,
+) {
+    let Some(model) = source_map.get(model_name) else {
+        panic!("{model_name}");
+    };
+
+    let mut current_winding = Winding::Ccw;
+    let mut current_inverted = ctx.inverted;
+
+    if ctx.transform.determinant() < 0.0 {
+        current_inverted = !current_inverted;
+    }
+
+    let mut invert_next = false;
+
+    for cmd in &model.cmds {
+        let effective_winding = if current_inverted {
+            !current_winding
+        } else {
+            current_winding
+        };
+
+        let mut push_triangle = |vertices, color| {
+            let color = new_color(ctx.color, color);
+
+            let vertices = ctx.project(vertices).map(bevy_from_weldr);
+            let mut tri = Triangle3d { vertices };
+            if effective_winding != Winding::Ccw {
+                tri.reverse();
+            }
+
+            if color != CURRENT_COLOR {
+                output.triangle_colors.insert(output.triangles.len(), color);
+            }
+
+            output.triangles.push(tri);
+        };
+
+        match cmd {
+            Command::Comment(c) => {
+                if c.text.starts_with("BFC CERTIFY") {
+                    current_winding = match &*c.text {
+                        "BFC CERTIFY CCW" => Winding::Ccw,
+                        "BFC CERTIFY CW" => Winding::Cw,
+                        _ => panic!("{}", c.text),
+                    };
+                } else if c.text.contains("BFC INVERTNEXT") {
+                    invert_next = true;
+                }
+            }
+            Command::SubFileRef(sfrc) => {
+                let child = ctx.child(sfrc, invert_next);
+                traverse_part(source_map, &sfrc.file, child, output);
+                invert_next = false;
+            }
+            Command::Line(l) => output
+                .lines
+                .push(ctx.project(l.vertices).map(bevy_from_weldr)),
+
+            Command::OptLine(l) => {
+                let [vertices, control_points] =
+                    [l.vertices, l.control_points].map(|x| ctx.project(x).map(bevy_from_weldr));
+                output.opt_lines.push((vertices, control_points));
+            }
+            Command::Triangle(t) => {
+                assert!(!invert_next);
+                push_triangle(t.vertices, t.color);
+            }
+            Command::Quad(q) => {
+                assert!(!invert_next);
+                let [a, b, c, d] = q.vertices;
+                push_triangle([a, b, c], q.color);
+                push_triangle([c, d, a], q.color);
+            }
+            _ => {}
+        }
+    }
+}
---
+++ render_3d/src/setup.rs
@@ -0,0 +1,213 @@
+use bevy_lines::prelude::*;
+use iyes_perf_ui::prelude::*;
+use ldr2pdf_common::{
+    ldr::{ColorCode, ColorMap, GeometryContext, new_color},
+    resolver::Resolver,
+};
+use std::collections::HashMap;
+use weldr::{Command, SourceMap};
+
+use bevy::{prelude::*, render::camera::Exposure};
+
+use crate::bevy_from_weldr_mat;
+use crate::primitives::Primitives;
+
+pub fn setup(
+    mut commands: Commands,
+    mut ambient_light: ResMut<AmbientLight>,
+
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+
+    mut lines: ResMut<Assets<Polyline>>,
+    mut line_materials: ResMut<Assets<PolylineMaterial>>,
+) {
+    let resolver = Resolver::new(dirs::document_dir().unwrap().join("lego/aria/HQ.io")).unwrap();
+    let mut source_map = SourceMap::new();
+    let main_model_name = weldr::parse("HQ.io", &resolver, &mut source_map).unwrap();
+
+    let color_map = ColorMap::load("C:/Program Files/Studio 2.0/ldraw/LDConfig.ldr").unwrap();
+
+    let mut ctx = GeometryContext::new();
+    ctx.transform = weldr::Mat4::from_rotation_z(std::f32::consts::PI)
+        * weldr::Mat4::from_scale(weldr::Vec3::splat(0.05));
+
+    let mut parts = Vec::new();
+    traverse_design(&source_map, &main_model_name, ctx.clone(), &mut parts);
+
+    let mut handles = Handles::default();
+    let line_material = line_materials.add(PolylineMaterial {
+        width: 3.0,
+        color: Color::BLACK.into(),
+        ..default()
+    });
+    let opt_line_material = line_materials.add(PolylineMaterial {
+        width: 6.0,
+        color: Color::BLACK.into(),
+        ..default()
+    });
+
+    for part in &parts {
+        handles.load_part(&source_map, &color_map, part, &mut meshes, &mut lines);
+        handles.load_material(&color_map, part.color, &mut materials);
+        handles.spawn_part(
+            &mut commands,
+            part,
+            line_material.clone(),
+            opt_line_material.clone(),
+        );
+    }
+
+    commands.spawn((
+        PointLight {
+            radius: 1.0,
+            ..default()
+        },
+        Transform::from_xyz(6.0, 8.0, -10.0),
+    ));
+    ambient_light.brightness = 220.0;
+
+    commands.spawn((
+        Camera3d::default(),
+        PerspectiveProjection {
+            far: 0.01,
+            ..default()
+        },
+        Exposure::INDOOR,
+        Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        bevy_flycam::FlyCam,
+    ));
+
+    commands.spawn(PerfUiAllEntries::default());
+}
+
+#[derive(Default)]
+struct Handles {
+    part: HashMap<String, PartHandles>,
+    material: HashMap<ColorCode, Handle<StandardMaterial>>,
+}
+
+#[derive(Clone)]
+struct PartHandles {
+    mesh: Handle<Mesh>,
+    line: Handle<Polyline>,
+    opt_line: Handle<Polyline>,
+}
+
+impl Handles {
+    fn load_part(
+        &mut self,
+        source_map: &SourceMap,
+        color_map: &ColorMap,
+        part: &Part,
+        meshes: &mut Assets<Mesh>,
+        lines: &mut Assets<Polyline>,
+    ) {
+        if self.part.contains_key(&part.id) {
+            return;
+        }
+
+        let primitives = Primitives::of_part(&source_map, &part.id);
+
+        self.part.insert(
+            part.id.clone(),
+            PartHandles {
+                mesh: meshes.add(primitives.build_mesh(&color_map, part.color)),
+                line: lines.add(primitives.build_lines()),
+                opt_line: lines.add(primitives.build_opt_lines()),
+            },
+        );
+    }
+
+    fn load_material(
+        &mut self,
+        color_map: &ColorMap,
+        part_color: ColorCode,
+        materials: &mut Assets<StandardMaterial>,
+    ) {
+        if self.material.contains_key(&part_color) {
+            return;
+        }
+
+        let ldraw_color = color_map.by_code(part_color);
+        let rgb = ldraw_color.value;
+        let alpha = ldraw_color.alpha.unwrap_or(0xFF);
+        let [r, g, b, a] = [rgb.red, rgb.green, rgb.blue, alpha].map(|n| n as f32 / 255.0);
+        // TODO: Any per-polygon colors are multiplied by the base color,
+        // but we want "overwrite" behavior, which will require a custom material.
+        let color = Color::srgba(r, g, b, a);
+        self.material.insert(part_color, materials.add(color));
+    }
+
+    fn spawn_part(
+        &self,
+        commands: &mut Commands,
+        part: &Part,
+        line_material: Handle<PolylineMaterial>,
+        opt_line_material: Handle<PolylineMaterial>,
+    ) {
+        let ph = self.part[&part.id].clone();
+
+        let material = MeshMaterial3d(self.material[&part.color].clone());
+        let line_material = PolylineMaterialHandle(line_material.clone());
+        let opt_line_material = PolylineMaterialHandle(opt_line_material.clone());
+
+        let transform = Transform::from_matrix(part.transform);
+
+        commands
+            .spawn((Mesh3d(ph.mesh), material.clone(), transform))
+            .with_children(|parent| {
+                parent.spawn(PolylineBundle {
+                    polyline: PolylineHandle(ph.line),
+                    material: line_material.clone(),
+                    ..default()
+                });
+
+                parent.spawn(PolylineBundle {
+                    polyline: PolylineHandle(ph.opt_line),
+                    material: opt_line_material.clone(),
+                    ..default()
+                });
+            });
+    }
+}
+
+struct Part {
+    id: String,
+    color: ColorCode,
+    transform: bevy::prelude::Mat4,
+}
+
+fn traverse_design(
+    source_map: &SourceMap,
+    model_name: &str,
+    ctx: GeometryContext,
+    output: &mut Vec<Part>,
+) {
+    let Some(model) = source_map.get(model_name) else {
+        panic!("{model_name}");
+    };
+
+    for cmd in &model.cmds {
+        match cmd {
+            Command::Comment(..) => {}
+            Command::SubFileRef(sfrc) => {
+                let child_ctx = ctx.child(sfrc, false);
+                if sfrc.file.ends_with(".dat") {
+                    let part = Part {
+                        id: sfrc.file.clone(),
+                        // TODO: respect currentcolor
+                        color: new_color(child_ctx.color, sfrc.color),
+                        transform: bevy_from_weldr_mat(child_ctx.transform),
+                    };
+                    output.push(part);
+                } else {
+                    traverse_design(source_map, &sfrc.file, child_ctx, output);
+                }
+            }
+            Command::Line(_) | Command::OptLine(_) => panic!("line in {model_name}"),
+            Command::Triangle(_) | Command::Quad(_) => panic!("polygon in {model_name}"),
+            _ => {}
+        }
+    }
+}
